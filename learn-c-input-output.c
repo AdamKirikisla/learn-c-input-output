ef896545063c42436dd975daea0fce7543ea3998
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-21 21:19:37 -0400

    add variable initialization

--- learn-c-input-output.c
+++ learn-c-input-output.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
+    printf("Job application form\n");
+    printf("Please fill in the application\n");
+
+    // variable initialization
+    char full_name[50] = "";
+    int age = 0;
+    char desired_position[50] = "";
+    float expected_salary = 0.0f;
+    char availability[50] = "";
+    bool is_authorized = false;
 
     return 0;
 }
